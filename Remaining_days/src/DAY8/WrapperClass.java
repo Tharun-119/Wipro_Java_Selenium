cee96a41b3f364bc45b55bae7f4c2c1e06f6d742
Tharun-119 <tharunkesireddy119@gmail.com>
2025-07-29 12:00:22 +0530

    Added

---
+++ Remaining_days/src/DAY8/WrapperClass.java
@@ -0,0 +1,10 @@
+package DAY8;
+
+public class WrapperClass {
+	public static void main(String[] args) {
+		String s = "25";
+		int i = Integer.parseInt(s);
+		System.out.println(i);
+	}
+
+}
---
+++ Remaining_days/src/DAY8/WrapperClass2.java
@@ -0,0 +1,9 @@
+package DAY8;
+
+public class WrapperClass2 {
+	public static void main(String[] args) {
+		String s = "123.45";
+		Double d = Double.parseDouble(s);
+		System.out.println(d);
+	}
+}
